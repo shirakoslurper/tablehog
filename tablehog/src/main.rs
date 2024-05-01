e5a46ecad04237fc3970ee73fad13edbb7c7cd6d
bonflower <54567022+asaphbay@users.noreply.github.com>
2024-05-01 00:23:08 -0700

    set local offset soundness to unsound

--- tablehog/src/main.rs
+++ tablehog/src/main.rs
@@ -7,6 +7,10 @@ use clap::Parser;
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {    
 
+    unsafe {
+        time::util::local_offset::set_soundness(time::util::local_offset::Soundness::Unsound);
+    }
+
     run().await?;
 
     Ok(())
